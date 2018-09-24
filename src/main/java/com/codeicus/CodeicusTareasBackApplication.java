0094774386968ff193aca2072956959daaa75f53
Larstack <larstackapp@gmail.com>
2018-09-24 20:16:40 -0300

    init

---
+++ src/main/java/com/codeicus/CodeicusTareasBackApplication.java
@@ -0,0 +1,12 @@
+package com.codeicus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeicusTareasBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeicusTareasBackApplication.class, args);
+	}
+}
